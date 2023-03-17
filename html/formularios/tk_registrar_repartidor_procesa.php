a1aa21345d58915b8c30cf0571b73256fcea6f6f
roprieto02 <rodrigo.prieto@upaep.edu.mx>
2023-03-17 11:31:14 -0600

    registrar repartidor

---
+++ html/formularios/tk_registrar_repartidor_procesa.php
@@ -0,0 +1,20 @@
+<p><?php
+	include("conex.php");
+	$link = Conectarse();
+
+	$nom = $_POST['nombre'];
+	$ap = $_POST['apellido'];
+	$cel = $_POST['celnum'];
+	$email = $_POST['correo'];
+	$sueldo = $_POST['sueldo'];
+	$comi = $_POST['comision'];
+
+	mysqli_query($link, "INSERT INTO tk_repartidores(nombre, apellidos, correo_electronico, celular, sueldo, comision)
+		VALUES('$nom', '$ap', '$email','$cel', '$sueldo', '$comi');") or die(mysqli_error($link));
+
+	mysqli_close($link);
+
+?></p>
+<script>
+    window.location.replace('tk_registrar_repartidor.php');
+</script>
