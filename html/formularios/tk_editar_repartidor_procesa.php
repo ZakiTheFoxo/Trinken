57809feecf9f641cf5b39fabc4a5ed99a67f6c06
roprieto02 <rodrigo.prieto@upaep.edu.mx>
2023-04-23 21:49:29 -0600

    editar repartidor(1, 2 y procesa)

--- html/formularios/tk_editar_repartidor_procesa.php
+++ html/formularios/tk_editar_repartidor_procesa.php
@@ -1,1 +1,22 @@
 
+<?php 
+    include("conex.php");
+    $link = Conectarse();
+
+    $id = $_POST['id'];
+    $nom = $_POST['nom_rep'];
+    $ape = $_POST['apellidos'];
+    $correo = $_POST['correo_electronico'];
+    $cel = $_POST['celular'];
+    $sue = $_POST['sueldo'];
+    $comi = $_POST['comision'];
+
+    mysqli_query($link, "UPDATE tk_repartidores SET
+    nombre = '$nom', apellidos = '$ape', correo_electronico = $correo, celular = '$cel', sueldo = $sue WHERE id = $id") or die(mysqli_error($link));
+
+    mysqli_close($link);   
+?>
+
+<script>
+    window.location.replace('tk_editar_repartidor.php?editarrepartidor=1');
+</script>
