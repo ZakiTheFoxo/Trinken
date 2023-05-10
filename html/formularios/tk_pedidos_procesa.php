722666c5841459b8811eb669eee9f3e937526d8f
DeathlineX <infinitydeathline@gmail.com>
2023-05-09 19:28:11 -0600

    Pedido Procesa

--- html/formularios/tk_pedidos_procesa.php
+++ html/formularios/tk_pedidos_procesa.php
@@ -4,6 +4,12 @@
 
     $id = $_POST['id'];
 
-    echo '$id';
-        
-?>
+    mysqli_query($link, "UPDATE tk_pedidos SET
+    estado = 'COMPLETADO', id = $id") or die(mysqli_error($link));
+
+    mysqli_close($link);   
+    ?>
+    <script>
+        alert('Pedido Completado');
+        window.history.go(-2);
+    </script>
